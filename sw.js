bbac5090344ecaa735e305404e0d874be04e0f67
fanis3645 <289221611+fanis3645@users.noreply.github.com>
2026-09-25 21:18:26 +0300

    Publish the a1 practice app so it opens on a phone.

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE="a1-1790360113142";
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["./", "./index.html", "./manifest.webmanifest"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((hit) => hit || fetch(event.request)));
+});
